bd299320bbdbf2af0bb875deed12847818302b34
Yann <yann.melo@gmail.com>
2021-06-10 13:00:34 -0300

    Main fetures finished

--- src/server.js
+++ src/server.js
@@ -6,16 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let postHomePage = [
-    {
-        id: 1,
-        title: 'Hello World',
-        coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-        contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-        content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-        commentCount: 2
-      }
-];
+let postHomePage = [];
+
+let commentID = [];
 
 app.get('/posts',(req,res)=>{
     res.send(postHomePage);
@@ -26,18 +19,33 @@ app.get('/posts/:postId', (req,res)=>{
     res.send(postHomePage.filter((i)=>i.id === id)[0]);
 });
 
-let countId = 1;
+let countId = 0;
 
 app.post('/posts', (req,res)=>{
     countId++;
     const post = req.body;
     post.content = post.content.replace("<p>","").replace("</p>","");
-    post.contentPreview = post.content.length > 19 ? post.content.split(20)[0] + "..." : post.content;
+    post.contentPreview = post.content.length > 15 ? post.content.split(16)[0] + "..." : post.content;
+    post.commentCount = 0;
     postHomePage.push({id:countId, ...post})
-    res.send("");
-    console.log(postHomePage);
+    res.sendStatus(200);
 });
 
+app.get('/posts/:id/comments',(req,res)=>{
+    const id = parseInt(req.params.id);
+    res.send(commentID.filter((i)=>i.postId === id))
+});
+
+app.post('/posts/:id/comments',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const comment = req.body;
+    comment.postId = id;
+    commentID.push(comment);
+    res.sendStatus(200);
+
+    const post = postHomePage.filter((i)=>i.id === id)[0]
+    post.commentCount++
+});
 
 app.listen(4000, ()=>{
     console.log("Servidor funcionando!")
